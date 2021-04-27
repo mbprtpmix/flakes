5408c89404d2a9032a944d421f0a01beddd3a90a
mbpnix <mbpnix@pm.me>
2021-04-27 06:55:27 +0300

    configuration.nix: Updates

--- configuration.nix
+++ configuration.nix
@@ -93,7 +93,7 @@
     uid = 1000;
     hashedPassword = "$6$vAnCJagYjSels0M$hmd87xvjT2QW2Wa2PAt5SI/yomr/pUgXEVe3Rx1SvHBxMDRlE5gmydhaMzUzPeWR9bpiB.6MYfGlpxZYc1MYc0";
     description = "MBPNIX";
-    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "networkmanager" "audio" "video" ]; # Enable ‘sudo’ for the user.
   };
 
   # List packages installed in system profile. To search, run:
@@ -120,12 +120,14 @@
       "https://cache.nixos.org"
       "https://cachix.cachix.org"
       "https://nix-community.cachix.org"
+      "https://fufexan.cachix.org"
       "https://mbpnix.cachix.org"
     ];
     binaryCachePublicKeys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "fufexan.cachix.org-1:LwCDjCJNJQf5XD2BV+yamQIMZfcKWR9ISIFy5curUsY="
       "mbpnix.cachix.org-1:nAfBijPdJRqcMhwDlIr4LbwPPKVWHROKx02Bcc/WbAI="
     ];
     trustedUsers = [ "root" "mbpnix" ];
