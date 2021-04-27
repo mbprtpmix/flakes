3466ca7ef1bc5e92718a0ac15b915964a2dee402
mbprtpmix <mbprtpmix@gmail.com>
2021-04-27 18:52:24 +0300

    updates: remove session path

--- hm/home.nix
+++ hm/home.nix
@@ -1,9 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ./modules/files.nix # files to link in ~
-  ];
+#  imports = [
+#    ./modules/files.nix # files to link in ~
+#  ];
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
@@ -15,10 +15,10 @@
   
   xdg.enable = true;
 
-  home.sessionPath = [
-    "${config.home.homeDirectory}/.local/bin"
-    "${config.home.homeDirectory}/.bin"
-  ];
+#  home.sessionPath = [
+#    "${config.home.homeDirectory}/.local/bin"
+#    "${config.home.homeDirectory}/.bin"
+#  ];
 
   home.sessionVariables = {
     EDITOR = "nvim";
